4eb7a0e46ef64abeb9b0765745ecf0bce44e5f22
Jonas Bach <hallo@jonasbach.de>
2023-03-20 13:37:58 +0100

    enhance CurrentTrack styles

--- src/components/CurrentTrack/CurrentTrack.tsx
+++ src/components/CurrentTrack/CurrentTrack.tsx
@@ -5,9 +5,13 @@ import {
   Box,
   Center,
   Flex,
+  HoverCard,
+  Image,
   Sx,
   Text,
+  Title,
   Tooltip,
+  rem,
 } from '@mantine/core';
 import { IconPlayerStopFilled } from '@tabler/icons-react';
 import { IconPhoto } from '@tabler/icons-react';
@@ -17,14 +21,24 @@ type CurrentTrackProps = { sx?: Sx };
 
 const CurrentTrack = ({ sx = {} }: CurrentTrackProps) => {
   const [currentTrack, setCurrentTrack] = useCurrentTrack();
+
+  // nothing playing, show fallback
   if (!currentTrack) {
     return (
       <Box
         sx={(theme) => ({
           borderRadius: theme.radius.md,
+          backgroundColor:
+            theme.colorScheme === 'dark'
+              ? theme.colors.dark[6]
+              : theme.colors.gray[0],
+          border: `${rem(1)} solid ${
+            theme.colorScheme === 'dark'
+              ? theme.colors.dark[4]
+              : theme.colors.gray[4]
+          }`,
           ...sx,
         })}
-        bg='gray.0'
         p='xs'
       >
         <Text c='dimmed' fz='sm' fw={500}>
@@ -40,23 +54,56 @@ const CurrentTrack = ({ sx = {} }: CurrentTrackProps) => {
         src={currentTrack.large_image}
         radius='md'
         sx={(theme) => ({
-          backgroundColor: theme.colors.gray[8],
+          border: `${rem(1)} solid ${
+            theme.colorScheme === 'dark'
+              ? theme.colors.dark[4]
+              : theme.colors.gray[4]
+          }`,
+          overflow: 'hidden',
         })}
       >
         <Center
           pr='xs'
-          sx={{
-            background: 'rgba(255,255,255,0.9)',
-          }}
+          sx={(theme) => ({
+            backgroundColor: theme.fn.rgba(
+              theme.colorScheme === 'dark'
+                ? theme.colors.dark[6]
+                : theme.colors.gray[0],
+              0.75
+            ),
+          })}
         >
           <Flex align='center'>
-            <Avatar
-              src={currentTrack.large_image}
-              alt={currentTrack.flavor_text}
-            >
-              <IconPhoto size='1.5rem' />
-            </Avatar>
-            <Text c='dimmed' fz='sm' ml='xs' mr='xs' fw={500}>
+            <HoverCard width={320} shadow='md' withArrow>
+              <HoverCard.Target>
+                <Avatar
+                  src={currentTrack.large_image}
+                  alt={currentTrack.flavor_text}
+                  radius='md'
+                  sx={{
+                    cursor: 'help',
+                  }}
+                >
+                  <IconPhoto size='1.5rem' />
+                </Avatar>
+              </HoverCard.Target>
+              <HoverCard.Dropdown>
+                <Text fz='xs' c='dimmed' mb='xs'>
+                  Currently playing:
+                </Text>
+                <Title order={5}>{currentTrack.track_title}</Title>
+                <Text size='sm' color='dimmed' mb='md'>
+                  {currentTrack.flavor_text}
+                </Text>
+                <Image
+                  src={currentTrack.large_image}
+                  alt={currentTrack.flavor_text}
+                  withPlaceholder
+                  radius='sm'
+                />
+              </HoverCard.Dropdown>
+            </HoverCard>
+            <Text fz='sm' ml='xs' mr='xs' fw={500}>
               {currentTrack.track_title}
             </Text>
             <Tooltip label='Stop playback for all listeners'>
